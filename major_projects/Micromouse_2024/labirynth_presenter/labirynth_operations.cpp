8470569a87a8932e9a2285024b5313df4b4100c2
purbans3 <pwm.urbanski@gmail.com>
2024-03-27 12:03:10 +0100

    File organisation update

---
+++ major_projects/Micromouse_2024/labirynth_presenter/labirynth_operations.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/* 	In this file there is a set of functions and solutions related to operations on
+	BMP files representing test labirynths for the Micromouse projects.				*/
+
+using namespace std;
+
+int main(){
+
+}
