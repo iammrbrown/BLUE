326a22b2cca1dde2469a7b18a2a0062453d4806f
purbans3 <pwm.urbanski@gmail.com>
2024-04-05 23:59:40 +0200

    TB progress: print_path and sample algorithm added

--- major_projects/Micromouse_2024/labirynth_operations/labirynth_operations.cpp
+++ major_projects/Micromouse_2024/labirynth_operations/labirynth_operations.cpp
@@ -17,10 +17,38 @@ typedef struct {
 	bool W;
 	bool START = false;
 	bool END = false;
+	int step = 0;
 } Tile;
 
+typedef struct{								
+	uint32_t 	size = 0;					// rozmiar pliku w bajtach				
+	uint32_t	reserved = 0; 				// można swobodnie wyzerować, nieważne w tym zastosowaniu
+	uint32_t	offset = 52;				// offset danych pikseli od początku pliku
+	uint32_t	dib_header_size = 40;		// długość headera informacji o bitmapie
+	int32_t 	width_px = LAB_SIZE*4+1;	// szerokość bitmapy (w pikselach)
+	int32_t 	height_px = LAB_SIZE*4+1;	// wysokość  bitmapy (w pikselach)
+	uint16_t	num_planes = 1;				// coś skomplikowanego, związanego z ilością płaszczyzn kolorów. jedna dla wygody.
+	uint16_t 	bits_per_pixel = 24;		// ilość bitów przeznaczanych na każdy piksel.
+	uint32_t	compression = 0;			// 0 dla naszych zastosowań
+	uint32_t	image_size_bytes = 0;	 	// przy compression = 0 ten parametr i tak jest ignorowany
+	int32_t 	x_resolution_ppm = 0; 		// znaczenie w druku - rozdzielczość w pikselach na metr X
+	int32_t 	y_resolution_ppm = 0; 		// znaczenie w druku - rozdzielczość w pikselach na metr X
+	uint32_t	num_colors = 0;				// po prawdzie - nie mam pojęcia
+	uint32_t 	important_colors = 0; 		// tu tak samo, ale działa
+}BMPHeader;
+
+typedef struct{
+	uint8_t R;
+	uint8_t G;
+	uint8_t B;
+}pixel;
+
 void import_labirynth (std::string, Tile [][LAB_SIZE]);
+void print_path (std::string, Tile [][LAB_SIZE]);
 void interpret_buffer (char [], char []); 
+void draw_wall (pixel [][LAB_SIZE*4+1], char, int, int);
+
+void A_right_handed (Tile [][LAB_SIZE]);
 
 int main (int argc, char *argv[]){
 
@@ -31,15 +59,77 @@ int main (int argc, char *argv[]){
 
 	Tile labirynth[LAB_SIZE][LAB_SIZE];
 	import_labirynth("./t_data/MM_TEST1.bmp", labirynth);
+ 	
+	A_right_handed(labirynth);
 
-	for(int y = LAB_SIZE-1; y >= 0; y--){
-		for(int x = 0; x < LAB_SIZE; x++){
-			std::cout << labirynth[x][y].W << " ";
-		}
-		std::cout << std::endl;
-	}
+	print_path("./path.bmp", labirynth);
 
 	return 0;
+}
+
+void A_right_handed (Tile labirynth[][LAB_SIZE]){
+	char facing = 'N';
+	char right = 'E';
+
+	int x = 0;
+	int y = 0;
+	int step = 0;
+
+	while(labirynth[x][y].END != true){
+		labirynth[x][y].step = step;
+
+		std::cout << "At: [ " << x << ", " << y << "]; Facing: " << facing << "; N: " << labirynth[x][y].N << ", E: " << labirynth[x][y].E << ", S: " << labirynth[x][y].S << ", W: " << labirynth[x][y].W << std::endl;
+		switch(facing){
+		case 'N':
+			right = 'E';
+			if(labirynth[x][y].E == true){
+				if(labirynth[x][y].N == false){y++;}
+				else{facing = 'W';}
+			}else{
+				facing = right;
+				x++;
+			}
+			break;
+		case 'E':
+			right = 'S';
+			if(labirynth[x][y].S == true){
+				if(labirynth[x][y].E == false){x++;}
+				else{facing = 'N';}
+			}else{
+				facing = right;
+				y--;
+			}
+			break;
+		case 'S':
+			right = 'W';
+			if(labirynth[x][y].W == true){
+				if(labirynth[x][y].S == false){y--;}
+				else{facing = 'E';}
+			}else{
+				facing = right;
+				x--;
+			}
+			break;
+		case 'W':
+			right = 'N';
+			if(labirynth[x][y].N == true){
+				if(labirynth[x][y].W == false){x--;}
+				else{facing = 'S';}
+			}else{
+				facing = right;
+				y++;
+			}
+			break;	
+		default:
+			std::cout << "Mamy problem: A_RH - BAD FACING" << std::endl;
+		}
+
+		std::cout << "Step: " << step << " Position: " << x << ", " << y << " Facing: " << facing << "\n";
+		if(x < 0 || x > 15 || y < 0 || y > 15){break;}
+		step++;
+		if(step >= 256){step = 0;}
+
+	}
 }
 
 void interpret_buffer (char in[], char out[]){
@@ -101,7 +191,7 @@ void import_labirynth (std::string path, Tile labirynth[][LAB_SIZE]){
 		data_file.seekp((int)buffer[0]+20, std::ios::beg); // +20 - preread first (black) line
 
 		for(int x = 0; x < LAB_SIZE; x++){
-			labirynth[x][0].S = false;
+			labirynth[x][0].S = true;
 		}
 
 		for(int y = 0; y < LAB_SIZE; y++){
@@ -120,14 +210,29 @@ void import_labirynth (std::string path, Tile labirynth[][LAB_SIZE]){
 			for(int x = 0; x < LAB_SIZE; x++){
 				switch(row[2*x]){
 				case 'B':
-					labirynth[x][y].E = true;
+					labirynth[x][y].W = true;
+					break;
+				case 'X':
+					std::cout << "Mamy problem: CASE_X: " << x << " " << y << std::endl;
 					break;
 				default:
-					labirynth[x][y].E = false;
+					labirynth[x][y].W = false;
 				}
-				if(x!=0){labirynth[x-1][y].W = labirynth[x][y].E;}
+				switch(row[2*x+1]){
+				case 'C': case 'W':
+					break;
+				case 'R':
+					labirynth[x][y].START = true;
+					break;
+				case 'G':
+					labirynth[x][y].END = true;
+					break;
+				default:
+					std::cout << "Mamy problem: LAB_CENTER_OFF: " << x << " " << y << std::endl;
+				}
+				if(x!=0){labirynth[x-1][y].E = labirynth[x][y].W;}
 			}
-			labirynth[LAB_SIZE-1][y].W = true;
+			labirynth[LAB_SIZE-1][y].E = true;
 
 			// TOP
 			data_file.read(buffer, 20);
@@ -137,6 +242,9 @@ void import_labirynth (std::string path, Tile labirynth[][LAB_SIZE]){
 				switch(row[2*x+1]){
 				case 'B':
 					labirynth[x][y].N = true;
+					break;
+				case 'X':
+					std::cout << "Mamy problem: CASE_X: " << x << " " << y << std::endl;
 					break;
 				default:
 					labirynth[x][y].N = false;
@@ -150,29 +258,85 @@ void import_labirynth (std::string path, Tile labirynth[][LAB_SIZE]){
 	}else std::cout << "The file bloody crashed! \n"; 
 }
 
-typedef struct{									
-	uint32_t 	size;				// rozmiar pliku w bajtach				
-	uint32_t	reserved; 			// można swobodnie wyzerować, nieważne w tym zastosowaniu
-	uint32_t	offset;				// offset danych pikseli od początku pliku
-	uint32_t	dib_header_size; 	// długość headera informacji o bitmapie
-	int32_t 	width_px;			// szerokość bitmapy (w pikselach)
-	int32_t 	height_px;			// wysokość  bitmapy (w pikselach)
-	uint16_t	num_planes;			// coś skomplikowanego, związanego z ilością płaszczyzn kolorów. jedna dla wygody.
-	uint16_t 	bits_per_pixel;		// ilość bitów przeznaczanych na każdy piksel.
-	uint32_t	compression;		// 0 dla naszych zastosowań
-	uint32_t	image_size_bytes; 	// przy compression = 0 ten parametr i tak jest ignorowany
-	int32_t 	x_resolution_ppm; 	// znaczenie w druku - rozdzielczość w pikselach na metr X
-	int32_t 	y_resolution_ppm; 	// znaczenie w druku - rozdzielczość w pikselach na metr X
-	uint32_t	num_colors;			// po prawdzie - nie mam pojęcia
-	uint32_t 	important_colors; 	// tu tak samo, ale działa
-}BMPHeader;
-
-typedef struct{
-	uint8_t B;
-	uint8_t G;
-	uint8_t R;
-}pixel;
-
-void print_bitmap (std::string path /*VARIABLE BITMAP SIZE?*/){	
+void draw_wall(pixel buffer[][LAB_SIZE*4+1], char side, int x, int y){
+	switch(side){
+	case 'N':
+		for(int i = 0; i < 5; i++){
+			buffer[4*x+i][4*(y+1)].R = 0;
+			buffer[4*x+i][4*(y+1)].G = 0;
+			buffer[4*x+i][4*(y+1)].B = 0;
+		}
+		break;
+	case 'E':
+		for(int i = 0; i < 5; i++){
+			buffer[4*(x+1)][4*y+i].R = 0;
+			buffer[4*(x+1)][4*y+i].G = 0;
+			buffer[4*(x+1)][4*y+i].B = 0;
+		}
+		break;
+	case 'S':
+		for(int i = 0; i < 5; i++){
+			buffer[4*x+i][4*y].R = 0;
+			buffer[4*x+i][4*y].G = 0;
+			buffer[4*x+i][4*y].B = 0;
+		}
+		break;
+	case 'W':
+		for(int i = 0; i < 5; i++){
+			buffer[4*x][4*y+i].R = 0;
+			buffer[4*x][4*y+i].G = 0;
+			buffer[4*x][4*y+i].B = 0;
+		}
+		break;
+	default:
+		std::cout << "Mamy problem: DRAW_WALL - BAD SIDE" << std::endl;
+	}
 }
 
+void print_path(std::string path, Tile labirynth[][LAB_SIZE]){
+
+	BMPHeader header;
+	uint16_t type = 0x4d42;	
+
+	pixel buffer[LAB_SIZE*4+1][LAB_SIZE*4+1];
+	uint8_t filler[3] = {0x69, 0x42, 0xff};
+
+	for(int x = 0; x < LAB_SIZE*4+1; x++){
+		for(int y = 0; y < LAB_SIZE*4+1; y++){
+			buffer[x][y].R = 255;
+			buffer[x][y].G = 255;
+			buffer[x][y].B = 255;
+		}
+	}
+
+	std::ofstream output;
+
+	output.open(path, std::ios::binary | std::ios::out);
+	if(output.good() == true){
+
+		for(int x = 0; x < LAB_SIZE; x++){
+			for(int y = 0; y < LAB_SIZE; y++){
+				if(labirynth[x][y].N == true){draw_wall(buffer, 'N', x, y);}
+				if(labirynth[x][y].E == true){draw_wall(buffer, 'E', x, y);}
+				if(labirynth[x][y].S == true){draw_wall(buffer, 'S', x, y);}
+				if(labirynth[x][y].W == true){draw_wall(buffer, 'W', x, y);}
+				buffer[4*x+1][4*y+1].G = 255-labirynth[x][y].step*3; buffer[4*x+2][4*y+1].G = 255-labirynth[x][y].step*3; buffer[4*x+3][4*y+1].G = 255-labirynth[x][y].step*3;
+				buffer[4*x+1][4*y+2].G = 255-labirynth[x][y].step*3; buffer[4*x+2][4*y+2].G = 255-labirynth[x][y].step*3; buffer[4*x+3][4*y+2].G = 255-labirynth[x][y].step*3;
+				buffer[4*x+1][4*y+3].G = 255-labirynth[x][y].step*3; buffer[4*x+2][4*y+3].G = 255-labirynth[x][y].step*3; buffer[4*x+3][4*y+3].G = 255-labirynth[x][y].step*3;
+			}
+		}
+
+		output.write((char *) &type, sizeof(uint16_t));
+		output.write((char *) &header, sizeof(BMPHeader));
+		std::cout << sizeof(BMPHeader) ;
+
+		for(int y = 0; y < LAB_SIZE*4+1; y++){
+			for(int x = 0; x < LAB_SIZE*4+1; x++){
+				output.write((char *) &buffer[x][y], sizeof(pixel));
+			}
+			if((LAB_SIZE*4+1)*3 % 4 != 0){
+				output.write((char *) &filler, (4-((LAB_SIZE*4+1)*3%4))*sizeof(uint8_t));
+			}
+		}
+	}
+}
